727ead45bb5f2d3f85e9ae9215a3c65f52d6163b
Nate O'Neill <noneill@patriotsoftware.com>
2021-08-20 10:27:01 -0400

    Update wording

--- action.sh
+++ action.sh
@@ -7,8 +7,8 @@ if [[ -n "${INPUT_BASE_URL_OVERRIDE}" ]]; then
     echo "::set-output name=resulting-base-url::$(grep "BASE_URL" ${INPUT_K6_SCRIPT} | head -1)"
 fi
 
-if [[ -n "${INPUT_ADDITIONAL_FILES}" ]]; then
-    docker run -v $(pwd)/${INPUT_K6_SCRIPT}:/k6/${INPUT_K6_SCRIPT} -v $(pwd)/${INPUT_ADDITIONAL_FILES}:/k6/${INPUT_ADDITIONAL_FILES} loadimpact/k6 run ${INPUT_K6_FLAGS} /k6/${INPUT_K6_SCRIPT}
+if [[ -n "${INPUT_ADDITIONAL_MOUNT}" ]]; then
+    docker run -v $(pwd)/${INPUT_K6_SCRIPT}:/k6/${INPUT_K6_SCRIPT} -v $(pwd)/${INPUT_ADDITIONAL_MOUNT}:/k6/${INPUT_ADDITIONAL_MOUNT} loadimpact/k6 run ${INPUT_K6_FLAGS} /k6/${INPUT_K6_SCRIPT}
 else
     docker run -v $(pwd)/${INPUT_K6_SCRIPT}:/k6/${INPUT_K6_SCRIPT} loadimpact/k6 run ${INPUT_K6_FLAGS} /k6/${INPUT_K6_SCRIPT}
 fi
