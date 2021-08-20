f8a841704a4fa4271bec590591f87535b2a3377a
Nate O'Neill <noneill@patriotsoftware.com>
2021-08-20 10:25:50 -0400

    Add optional files directory to be used by the K6 script

--- action.sh
+++ action.sh
@@ -6,4 +6,9 @@ if [[ -n "${INPUT_BASE_URL_OVERRIDE}" ]]; then
     sed -i "s,const BASE_URL =.*,const BASE_URL = \"${INPUT_BASE_URL_OVERRIDE}\";,g" $INPUT_K6_SCRIPT
     echo "::set-output name=resulting-base-url::$(grep "BASE_URL" ${INPUT_K6_SCRIPT} | head -1)"
 fi
-docker run -v $(pwd)/${INPUT_K6_SCRIPT}:/k6/${INPUT_K6_SCRIPT} loadimpact/k6 run ${INPUT_K6_FLAGS} /k6/${INPUT_K6_SCRIPT}
+
+if [[ -n "${INPUT_ADDITIONAL_FILES}" ]]; then
+    docker run -v $(pwd)/${INPUT_K6_SCRIPT}:/k6/${INPUT_K6_SCRIPT} -v $(pwd)/${INPUT_ADDITIONAL_FILES}:/k6/${INPUT_ADDITIONAL_FILES} loadimpact/k6 run ${INPUT_K6_FLAGS} /k6/${INPUT_K6_SCRIPT}
+else
+    docker run -v $(pwd)/${INPUT_K6_SCRIPT}:/k6/${INPUT_K6_SCRIPT} loadimpact/k6 run ${INPUT_K6_FLAGS} /k6/${INPUT_K6_SCRIPT}
+fi
