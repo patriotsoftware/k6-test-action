00ac24a2ab4dff41b28f84e684e34c43fdb76eba
joshfng <121190566+joshfng@users.noreply.github.com>
2024-09-05 11:07:37 -0400

    Debug

--- action.sh
+++ action.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-set -e
-if [[ -n "${INPUT_BASE_URL_OVERRIDE}" ]]; then 
+set -exv
+
+if [[ -n "${INPUT_BASE_URL_OVERRIDE}" ]]; then
     echo "initial-base-url=$(grep "BASE_URL" ${INPUT_K6_SCRIPT} | head -1)" >> $GITHUB_OUTPUT
     sed -i "s,const BASE_URL =.*,const BASE_URL = \"${INPUT_BASE_URL_OVERRIDE}\";,g" $INPUT_K6_SCRIPT
     echo "resulting-base-url=$(grep "BASE_URL" ${INPUT_K6_SCRIPT} | head -1)" >> $GITHUB_OUTPUT
